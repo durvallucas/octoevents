8ba3760bb211603e2b55971653a49c61a8732a9b
lucas <lucas@DESKTOP-9R4V64N>
2019-04-07 15:36:04 -0300

    Projeto Octo Events

---
+++ src/main/java/com/example/octoevents/OctoeventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.octoevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoeventsApplication.class, args);
+	}
+
+}
